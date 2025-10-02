fa6f3ec2e6f518eca46c0ea3475fb6164ec9e80c
josinevanmarrewijkVHGM <162108923+josinevanmarrewijkVHGM@users.noreply.github.com>
2025-10-02 11:42:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -448,14 +448,14 @@ def plot_monthly_temperature(
         
 from PIL import Image
 
-def add_logo(fig, zoom, logo_path="assets/logo.png", position=(0.85, 0.85)):
+def add_logo(fig, zoom, logopath="assets/logo.png", position=(0.85, 0.85)):
     """
     Voeg een logo toe aan een matplotlib figuur met een zoomfactor.
     Als het logo niet gevonden wordt, gebruik dan een fallback-logo.
     """
-    if os.path.exists(logo_path):
+    if os.path.exists(logopath):
         # Lees het logo in met PIL en pas zoom toe
-        logo_img = Image.open(logo_path)
+        logo_img = Image.open(logopath)
         width, height = logo_img.size
         new_size = (int(width * zoom), int(height * zoom))
         logo_img = logo_img.resize(new_size, Image.ANTIALIAS)
@@ -472,8 +472,8 @@ def add_logo(fig, zoom, logo_path="assets/logo.png", position=(0.85, 0.85)):
             zorder=10
         )
     else:
-        print("Bestaat bestand?", os.path.exists(logo_path))
-        print(f"❌ Geen logo beschikbaar. Controleer pad: {logo_path}")
+        print("Bestaat bestand?", os.path.exists(logopath))
+        print(f"❌ Geen logo beschikbaar. Controleer pad: {logopath}")
         return
     # logo = mpimg.imread(logo_path)
     # ax_logo = fig.add_axes([position[0], position[1], zoom, zoom], anchor='NE', zorder=1)
